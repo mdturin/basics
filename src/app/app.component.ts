1d1c9039ad02e1c37f186f0fe3cf2c830f19c770
Md Turin <matkhan01@yahoo.com>
2023-03-18 23:04:25 +0600

    5-Transforming Content Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,29 @@ import { Component } from "@angular/core";
   ]
 })
 export class AppComponent implements OnInit, OnChanges {
-  name = 'Md Turin';
+  name = 'md turin';
   imgURL = 'https://picsum.photos/id/237/500/500';
+  images = [
+    this.imgURL, this.imgURL, this.imgURL
+  ]
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large'
+  }
+
+  blueClass = false;
+  fontSize = 10;
 
   constructor(){
     console.log('app constructor() called...');
+  }
+
+  changeButtonProperty(){
+    this.blueClass = !this.blueClass;
+    this.fontSize = 30 - this.fontSize;
   }
 
   ngOnInit(){
